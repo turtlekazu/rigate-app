75e9eb3a5efaf78a8fc0180eafb5d64fa5b309db
turtlekazu <turtlekazu@icloud.com>
2021-03-08 15:56:12 +0900

    Add rubocop configuration, fix offences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
