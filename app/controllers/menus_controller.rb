cee33513ea7ced80c1700ac9b96cad97469013a3
turtlekazu <turtlekazu@icloud.com>
2021-03-09 19:29:59 +0900

    Add menus#index, #new, #create

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,24 @@
 class MenusController < ApplicationController
   def index
+    @menus = Menu.all
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :pasta_name, :image)
   end
 end
