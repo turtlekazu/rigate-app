ccdbfbac2f59a03e83ef1684b2868ebb4e751953
turtlekazu <turtlekazu@icloud.com>
2021-03-11 00:06:45 +0900

    Add associations

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,7 +1,8 @@
 class Curriculum < ApplicationRecord
   belongs_to :menu
-  has_many   :contents, dependent: :destroy
-  has_many   :exams,    dependent: :destroy
+  has_many   :contents,       dependent: :destroy
+  has_one    :exams,          dependent: :destroy
+  has_many   :exam_responses, dependent: :destroy
 
   validates :name, presence: true
 end
