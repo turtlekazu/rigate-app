f1cd354b6a1cd91aadf324d0aab6a5d8bf48c74a
turtlekazu <turtlekazu@icloud.com>
2021-03-11 18:22:48 +0900

    Add welcome routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'menus#index'
-  resources :menus
+
+  resources :menus do
+    collection do
+      get 'welcome'
+    end
+  end
   resources :curriculums do
     resources :contents,       only: [:new, :create]
     resources :exams,          only: [:new, :create]
