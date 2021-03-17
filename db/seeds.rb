e7bf8d91532fd34c93bc28103c65cc6985a02c58
turtlekazu <turtlekazu@icloud.com>
2021-03-17 23:47:55 +0900

    Add seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+menu1 = Menu.new(name: "見た目の改善", pasta_name: "FarFalle")
+menu1.image.attach(io: File.open('app/assets/images/FarFalle.png'), filename: 'FarFalle.png')
+menu1.save!
+
+menu2 = Menu.new(name: "ループとロジック", pasta_name: "Fusilli")
+menu2.image.attach(io: File.open('app/assets/images/Fusilli.png'), filename: 'Fusilli.png')
+menu2.save!
+
+menu3 = Menu.new(name: "コードの分割・再構成", pasta_name: "Penne")
+menu3.image.attach(io: File.open('app/assets/images/Penne.png'), filename: 'Penne.png')
+menu3.save!
+
+menu4 = Menu.new(name: "テスト・エラー処理", pasta_name: "Conchiglie")
+menu4.image.attach(io: File.open('app/assets/images/Conchiglie.png'), filename: 'Conchiglie.png')
+menu4.save!
+
+Curriculum.create!(name: "名前のつけ方", menu_id: 1)
+Curriculum.create!(name: "フォーマット", menu_id: 1)
+Curriculum.create!(name: "コメント", menu_id: 1)
+Curriculum.create!(name: "if文・条件分岐", menu_id: 2)
+Curriculum.create!(name: "巨大な式の分割", menu_id: 2)
+Curriculum.create!(name: "変数を洗練させる", menu_id: 2)
+Curriculum.create!(name: "無関係な下位関係の抽出", menu_id: 3)
+Curriculum.create!(name: "1度に一つのことを行う", menu_id: 3)
+Curriculum.create!(name: "コードを言葉で説明する", menu_id: 3)
+Curriculum.create!(name: "単体テスト", menu_id: 4)
+Curriculum.create!(name: "結合テスト", menu_id: 4)
+Curriculum.create!(name: "エラー処理", menu_id: 4)
+
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 1)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 2)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 3)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 4)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 5)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 6)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 7)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 8)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 9)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 10)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 11)
+Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: 12)
+
+User.create!(nickname: "admin", email: "admin@admin", password: "adminadmin", password_confirmation: "adminadmin", admin: 1)
