3bbcea44727c2964c7afb21b3c455bcae3f81a73
turtlekazu <turtlekazu@icloud.com>
2021-03-19 11:50:28 +0900

    Add markedjs & highlightjs

--- app/javascript/codeblock.js
+++ app/javascript/codeblock.js
@@ -1,16 +1,16 @@
-import hljs from 'highlight.js';
+import highlight from 'highlight.js';
 
 function markdownCode() {
-  const codeInput = document.getElementById('user_answer_code');
-  const codeMarkdown = document.getElementById('code_markdown');
+  let codeInput = document.getElementById('user_answer_code');
+  let codeMarkdown = document.getElementById('code_markdown');
   if (codeInput == null || codeMarkdown == null) {
     return;
   }
 
   codeInput.addEventListener('input', () => {
-    const HTML = codeInput.value;
-    codeMarkdown.innerHTML = marked(HTML);
-    hljs.initHighlighting();
+    let HTML = "```ruby\n" + `${codeInput.value}` + "\n```";
+    let res = highlight.highlightAuto(marked(HTML));
+    codeMarkdown.innerHTML = res.value;
   });
 };
 
