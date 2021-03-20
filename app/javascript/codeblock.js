6c07412eb8d4ed2c34e61bd5cf540a71c4b2ec75
turtlekazu <turtlekazu@icloud.com>
2021-03-20 12:43:59 +0900

    Change let to const

--- app/javascript/codeblock.js
+++ app/javascript/codeblock.js
@@ -1,15 +1,15 @@
 function markdownCode() {
-  let codeInput = document.getElementById('user_answer_code');
-  let codeMarkdown = document.getElementById('code_markdown');
+  const codeInput = document.getElementById('user_answer_code');
+  const codeMarkdown = document.getElementById('code_markdown');
   if (codeInput == null || codeMarkdown == null) {
     return;
   }
 
   codeInput.addEventListener('input', () => {
-    let HTML = `${codeInput.value}`;
+    const HTML = `${codeInput.value}`;
     codeMarkdown.innerHTML = marked(HTML);
-    var pre_code_nodes = document.querySelectorAll("pre code");
-    for(var i = 0; i < pre_code_nodes.length; ++i){
+    const pre_code_nodes = document.querySelectorAll("pre code");
+    for(let i = 0; i < pre_code_nodes.length; ++i){
       hljs.highlightBlock(pre_code_nodes[i]);
     }
   });
