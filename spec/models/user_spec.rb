715aa87593c0f2041c466f18af50c1d30a15b260
turtlekazu <turtlekazu@icloud.com>
2021-03-24 16:38:35 +0900

    Add user_spec test_code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    context 'Success' do
+       it 'nickname, email, password, 確認用passwordが適切に入力されていて、無事に登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'Fail' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'emailに"@"がないと登録できない' do
+        @user.email = "foge"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+
+      it 'passwordが空では登録できない' do
+        @user.password = ""
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = "hoge"
+        @user.password_confirmation = "hoge"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it 'passwordと確認用passwordが不一致だと登録できない' do
+        @user.password = "hogehoge"
+        @user.password_confirmation = "fugafuga"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
 end
