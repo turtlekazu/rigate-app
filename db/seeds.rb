3ccac6d37bd57fd1cd23bf2bd947caa25ba46f3b
turtlekazu <turtlekazu@icloud.com>
2021-03-26 15:02:28 +0900

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,37 @@ Curriculum.create!(name: "単体テスト", menu_id: menu.id)
 Curriculum.create!(name: "結合テスト", menu_id: menu.id)
 Curriculum.create!(name: "エラー処理", menu_id: menu.id)
 
+
+
 curriculum = Curriculum.find_by(name: "名前のつけ方")
+Content.create(title: "1. 明確な単語を選ぶ", text: '「名前に情報を詰め込む」ことを意識しましょう。
+  getやsize、stopといった単語は抽象的すぎて、具体的な動作が想像できません。
+
+  getなら、fetchやdownloadなどに。
+  sizeなら中身に合わせて、heightやNumNodes,MemoryBytesなどに。
+  stopなら、取り消せない場合はkill, 後から戻せるならpauseなどに。
+  類義語をできるだけ多く知識として身につけ、「カラフル」な単語を使いましょう。
+  
+  sendの類義語 → deliver, dispatch, announce, distribute, route
+  findの類義語 → search, extract, locate, recover
+  startの類義語 → launch, create, begin, open
+  makeの類義語 → create, set up, build, generate, compose, add, new', curriculum_id: curriculum.id)
+Content.create(title: "2. 汎用的な名前を避ける", text: 'fogeやfoo, value, tmpといった、何にでも使える名前をつけることは、情報を埋め込む余地を逃してしまい、非常にもったいないことです。エンティティのもつ具体的な"値"や"目的"を表した名前をつけましょう。
+
+  例えば、その「value」はある値の2乗の合計を表しているのだとすれば、「sum_squares」と名付けたり。
+  生存期間が数行で他の役割がない場合に、あえて「tmp」という名前を選ぶのなら、それはきちんと情報を伝えているのでOKです。', curriculum_id: curriculum.id)
+Content.create(title: "3. ループ処理", text: 'i, j, kといった名前はループ処理の内部変数としてよく使います。これ自体は「イテレータ」という情報をちゃんと伝えています。
+
+  しかし、イテレータが複数ある場合はもっといいアイデアがあります。例えばi, j, kを、club_i, menbers_i, users_iのように、異なる種類のイテレータには異なる言葉を頭につけます。こうすればj,kを使う必要がありません！', curriculum_id: curriculum.id)
+Content.create(title: "4. 超具体的な名前をつける", text: '例えば任意のTCP/IPポートをサーバがリッスンできるか確認するメソッドに「ServerCanStart()」という名前をつけたとします。この名前はまだ抽象的です。
+
+Serverは何をStartするのか、そのために何を確認しているのか？その戻り値は何なのか？
+例えば「CanListenOnPort()」という名前にすれば万事解決です。', curriculum_id: curriculum.id)
+Content.create(title: "5. 単位や属性をつける", text: '名前は短いコメントのようなものです。単位の情報や、重要な属性は直接名前につけてしまいましょう。
+
+  例えば、16進数のIDなら「id」ではなく「hex_id」に。
+  秒数は"秒"か"ミリ秒"かも重要な情報です。ミリ秒の経過時間を表すなら「elapsed」ではなく「elapsed_ms」としましょう。
+  他にも、暗号化をする必要があるプレーンテキストのパスワードが格納された変数がある場合、「password」ではなく「plaintext_password」の方が将来のミスを防げます。', curriculum_id: curriculum.id)
 Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: curriculum.id)
 curriculum = Curriculum.find_by(name: "フォーマット")
 Exam.create!(question: "以下のコードを分かりやすく命名してください", question_code: "def test\nputs question\nend", answer_code:"def test\nputs answer\nend", explanation:"explain", curriculum_id: curriculum.id)
